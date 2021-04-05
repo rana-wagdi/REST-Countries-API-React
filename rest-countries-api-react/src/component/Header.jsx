82b7c91c0078ed40ec2a30c94981588f9d444da7
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-04-04 17:34:25 -0700

    grid

---
+++ rest-countries-api-react/src/component/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div>
+        <div>
+            <h1>Where in the world?</h1>
+        </div>
+        <div>
+             <i className="fas fa-moon"> Dark Mode</i> 
+             </div>
+        </div>
+        </div>
+    )
+}
+
+export default Header;
