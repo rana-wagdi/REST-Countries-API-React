cca23cba1b5b89e2856623ef6479261fe6c98ab0
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-04-07 02:30:24 -0700

    add mode

--- rest-countries-api-react/src/App.js
+++ rest-countries-api-react/src/App.js
@@ -1,20 +1,51 @@
 import React from 'react';
 import Header from './component/Header';
-
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 import Countries from './component/Countries';
 
 
+const GlobalStyle = createGlobalStyle`
+body, img{
+  background-color: ${(props) =>
+    props.theme.mode === "dark" ? "hsl(207, 26%, 17%)" : " hsl(0, 0%, 98%)"};
+  color: ${(props) =>
+    props.theme.mode === "dark" ? " hsl(0, 0%, 98%)" : "hsl(200, 15%, 8%)"};
+}
+.header, #search, .select, .article-element{ 
+background-color: ${(props) =>
+  props.theme.mode === "dark" ? "hsl(209, 23%, 22%)" : " hsl(0, 0%, 100%)"};
+ 
+}
+body, #search, input::placeholder, .select, .article-element{
+  color: ${(props) =>
+    props.theme.mode === "dark" ? " hsl(0, 0%, 100%)" : "hsl(200, 15%, 8%)"};
+}
+.header, .article-element{
+  border-bottom-color: ${(props) =>
+    props.theme.mode === "dark" ? "hsl(209, 23%, 22%)" : "#ded9ca"};
+}
+#search, .select, #search:focus, .select:focus, img{
+  border-color: ${(props) =>
+    props.theme.mode === "dark" ? "hsl(209, 23%, 22%)" : "#ded9ca"};
+    box-shadow: ${(props) =>
+      props.theme.mode === "dark" ? "hsl(209, 23%, 22%)" : "#ded9ca"};
+}
+`;
 
 function App() {
   
   
     return (
-      <div>
-        <Header />
-        
-        <Countries />
-      </div>
-    )
+      <ThemeProvider theme={{ mode: "light" }}>
+        <div>
+          <GlobalStyle />
+            <Header />
+
+            <Countries />
+         
+        </div>
+      </ThemeProvider>
+    );
 
 }
 
